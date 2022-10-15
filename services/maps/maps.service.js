88bb1572982c15501331f84a346af28ae2d83e54
Mustafa Lopaev <mdlopayev@gmail.com>
2022-10-15 19:56:24 +0600

    Updated get method for maps

--- services/maps/maps.service.js
+++ services/maps/maps.service.js
@@ -1,33 +1,33 @@
 const axios = require('axios');
 
 const { calculate_carbon_emissions } = require('./utilities');
+const default_config = {
+  method: 'get',
+  url: process.env.CUSTOMCONNSTR_GOOGLE_URL,
+  headers: {},
+};
+
+const default_params = {
+  key: process.env.CUSTOMCONNSTR_GOOGLE_KEY,
+  alternatives: true,
+};
 
 // Get method with url filters inserted
-const get_road_transit = () => async (context, req) => {
-  var params = {
-    ...req.query,
-    key: process.env.CUSTOMCONNSTR_GOOGLE_KEY,
-    alternatives: true,
-    mode: 'transit',
-  };
-
+const get_road = () => async (context, req) => {
   var config = {
-    method: 'get',
-    url: process.env.CUSTOMCONNSTR_GOOGLE_URL,
-    headers: {},
-    params: params,
+    ...default_config,
+    params: { ...req.query, ...default_params },
   };
-
   return axios(config)
     .then(function (response) {
-      response.data.routes = calculate_carbon_emissions(response.data.routes);
-      context.res = { status: 200, body: response.data };
+      context.res = {
+        status: 200,
+        body: calculate_carbon_emissions(response.data),
+      };
     })
     .catch(function (error) {
       context.res = { status: 500, body: error };
     });
 };
 
-module.exports = {
-  get_road_transit,
-};
+module.exports = { get_road };
--- services/maps/utilities.js
+++ services/maps/utilities.js
@@ -1,48 +1,24 @@
-// Carbon emission gram per km
-const carbon_emission_per_meter = {
-  BUS: 0.0966,
-  WALKING: 0.0002,
-  SUBWAY: 0.0034,
-  CAR: 0.166,
-  METRO_RAIL: 0.0034,
-  CABLE_CAR: 0.166,
-  COMMUTER_TRAIN: 0.0034,
-  FERRY: 0.02,
-  FUNICULAR: 0.166,
-  GONDOLA_LIFT: 0.0034,
-  HEAVY_RAIL: 0.0034,
-  HIGH_SPEED_TRAIN: 0.0034,
-  INTERCITY_BUS: 0.0966,
-  LONG_DISTANCE_TRAIN: 0.0034,
-  MONORAIL: 0.02,
-  OTHER: 0.05,
-  RAIL: 0.0034,
-  SHARE_TAXI: 0.166,
-  TRAM: 0.0034,
-  TROLLEYBUS: 0.0034,
-};
+const { CARBON_EMISSION_PER_METER, FIXED_TRAVEL_MODES } = require('./config');
 
 // Used to calculate the carbon offset of the road
 function CO2(steps) {
   var total_emission = 0;
-  steps.map(
-    (step) =>
-      (total_emission +=
-        carbon_emission_per_meter[
-          step.travel_mode === 'WALKING'
-            ? 'WALKING'
-            : step.transit_details.line.vehicle.type
-        ] * step.distance.value)
-  );
+  steps.map((step) => {
+    total_emission +=
+      CARBON_EMISSION_PER_METER[
+        FIXED_TRAVEL_MODES.includes(step.travel_mode)
+          ? step.travel_mode
+          : step.transit_details.line.vehicle.type
+      ] * step.distance.value;
+  });
   return parseFloat((total_emission / 1000).toFixed(2));
 }
 
-function calculate_carbon_emissions(routes) {
-  routes.map((route) => (route.co2 = CO2(route.legs[0].steps)));
-  return routes;
+function calculate_carbon_emissions(data) {
+  data.routes.map((route) => (route.co2 = CO2(route.legs[0].steps)));
+  return data;
 }
 
 module.exports = {
-  CO2,
   calculate_carbon_emissions,
 };
