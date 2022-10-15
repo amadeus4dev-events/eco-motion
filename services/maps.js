4def08bce4d6322de398514628c5d6adf81b3254
Mustafa Lopaev <mdlopayev@gmail.com>
2022-10-15 13:11:59 +0600

    Updated gitignore

--- services/maps.js
+++ services/maps.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 // Get method with url filters inserted
 const get_road = () => async (context, req) => {
+  var params = { ...req.query, key: process.env.CUSTOMCONNSTR_GOOGLE_KEY}
+  
   var config = {
     method: 'get',
     url: process.env.CUSTOMCONNSTR_GOOGLE_URL,
     headers: {},
-    params: { ...req.query, key: process.env.CUSTOMCONNSTR_GOOGLE_KEY},
+    params: params,
   };
 
   return axios(config)
